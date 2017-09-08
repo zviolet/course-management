59c4c9511925cda95a780a4bd445846559528c25
zviolet <185740626@qq.com>
2017-09-08 18:37:11 +0800

    Create Course.java

---
+++ src/main/java/com/zviolet/entity/Course.java
@@ -0,0 +1,94 @@
+package com.zviolet.entity;
+
+public class Course {
+    //课程id
+    private Integer courseId;
+    //课程名
+    private String courseName;
+    //教室id
+    private Integer teacherId;
+    //上课时间
+    private String courseTime;
+    //上课地点
+    private String courseRoom;
+    //周数
+    private Integer courseWeek;
+    //课程类型
+    private String courseType;
+    //学院id
+    private Integer collegeId;
+    //学分
+    private Integer score;
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getCourseTime() {
+        return courseTime;
+    }
+
+    public void setCourseTime(String courseTime) {
+        this.courseTime = courseTime;
+    }
+
+    public String getCourseRoom() {
+        return courseRoom;
+    }
+
+    public void setCourseRoom(String courseRoom) {
+        this.courseRoom = courseRoom;
+    }
+
+    public Integer getCourseWeek() {
+        return courseWeek;
+    }
+
+    public void setCourseWeek(Integer courseWeek) {
+        this.courseWeek = courseWeek;
+    }
+
+    public String getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(String courseType) {
+        this.courseType = courseType;
+    }
+
+    public Integer getCollegeId() {
+        return collegeId;
+    }
+
+    public void setCollegeId(Integer collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
