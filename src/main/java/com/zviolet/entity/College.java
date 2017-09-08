b59a8efa9664bd043373dad437499af08b02cf9d
zviolet <185740626@qq.com>
2017-09-08 20:27:58 +0800

    Create College.java

---
+++ src/main/java/com/zviolet/entity/College.java
@@ -0,0 +1,24 @@
+package com.zviolet.entity;
+
+public class College {
+    //学院id
+    private Integer collegeId;
+    //学院名称
+    private String collegeName;
+
+    public Integer getCollegeId() {
+        return collegeId;
+    }
+
+    public void setCollegeId(Integer collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+}
