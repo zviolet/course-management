61049d2a35fc90d47953b3f47b52e81f0505e395
zviolet <185740626@qq.com>
2017-09-14 20:19:31 +0800

    Update StudentDao.java

--- src/main/java/com/zviolet/dao/StudentDao.java
+++ src/main/java/com/zviolet/dao/StudentDao.java
@@ -3,6 +3,9 @@ package com.zviolet.dao;
 import com.zviolet.entity.Student;
 
 public interface StudentDao {
+     
+    List<Student> findByPaging(PagingVO pagingVO) throws Exception;
+
     /**
      * 插入一条学生记录
      * @param record
@@ -15,5 +18,18 @@ public interface StudentDao {
      * @param record
      * @return
      */
-    int updateById(Student record);
+    void updateById(Student record);
+
+    /**
+     * 根据id查询学生信息
+     * @param id
+     * @return
+     */
+    Student selectById(Integer id);
+
+    /**
+     * 根据id删除学生信息
+     * @param id
+     */
+    void deleteById(Integer id);
 }
