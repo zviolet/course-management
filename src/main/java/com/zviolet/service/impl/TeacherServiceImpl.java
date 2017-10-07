26d6862ce553bccc0a9ef5f7ca763a02f84dd3c8
zviolet <185740626@qq.com>
2017-10-07 23:44:32 +0800

    Create TeacherServiceImpl.java

---
+++ src/main/java/com/zviolet/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,36 @@
+import com.zviolet.dao.TeacherDao;
+import com.zviolet.entity.PagingVO;
+import com.zviolet.entity.Teacher;
+import com.zviolet.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TeacherServiceImpl implements TeacherService{
+
+    @Autowired
+    private TeacherDao teacherDao;
+
+    public List<Teacher> findAll() {
+        return teacherDao.selectAll();
+    }
+
+    public List<Teacher> findByPaging(Integer page) throws Exception {
+        PagingVO pagingVO = new PagingVO();
+        pagingVO.setShowPage(page);
+        pagingVO.setShowRecordNo((page - 1) * pagingVO.getPageSize());
+        List<Teacher> teacherList = teacherDao.findByPaging(pagingVO);
+        return teacherList;
+    }
+
+    public boolean save(Teacher teacher) throws Exception {
+        Teacher tea = teacherDao.selectById(teacher.getTeacherId());
+        if (tea == null) {
+            teacherDao.insert(teacher);
+            return true;
+        }
+        return false;
+    }
+}
